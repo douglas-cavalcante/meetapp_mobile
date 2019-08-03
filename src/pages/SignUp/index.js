105776524e65792798737bca0d66353cafa6efd0
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-03 12:45:43 -0300

    Adiciona tela de SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
+import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
-  return <Text>Sign Uo</Text>;
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {}
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome Completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <FormInput
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <SubmitButton onPress={() => {}}>Criar Conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho Login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavion: 'padding',
+})`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const Form = styled.View`
+  align-self: stretch;
+  margin-top: 50px;
+`;
+
+export const FormInput = styled(Input)`
+  margin-top: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 15px;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+export const SignLinkText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
