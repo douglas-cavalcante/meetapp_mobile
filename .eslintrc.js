024c78847d43ad9c7d2f7c309383731170089ff7
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-02 22:52:11 -0300

    Adiciona configurações

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
+    'import/no-unresolved': 'off',
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
