3e3af641614f99d395385845def6171d141e69f0
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-05 18:41:48 -0300

    Adiciona componente Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'react-native';
+import logo from '~/assets/logo.png';
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Image source={logo} />
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 64px;
+  background-color: #000000;
+  justify-content: center;
+  align-items: center;
+`;
