e9d87c3faa8f14943cbe34690e586befb0719338
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-08 15:00:58 -0300

    Adiciona componente da tela de listagem de inscrições

--- src/pages/Subscriptions/index.js
+++ src/pages/Subscriptions/index.js
@@ -1,11 +1,63 @@
-import React from 'react';
-import { View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import { Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Header from '~/components/Header';
+
+import { List } from './styles';
+import Background from '~/components/Background';
+import Card from '~/components/Card';
+import api from '~/services/api';
 
 export default function Subscriptions() {
-  return <View />;
+  const [meetups, setMeetups] = useState([]);
+
+  async function loadMeetups() {
+    const response = await api.get('subscriptions');
+    const subscriptions = response.data.map(subscription => ({
+      id: subscription.id,
+      File: {
+        ...subscription.Meetup.File,
+      },
+      User: {
+        ...subscription.Meetup.User,
+      },
+      date: subscription.Meetup.date,
+      location: subscription.Meetup.location,
+      title: subscription.Meetup.title,
+    }));
+    setMeetups(subscriptions);
+  }
+
+  useEffect(() => loadMeetups(), []);
+
+  async function handleCancel(id) {
+    try {
+      await api.delete(`/subscriptions/${id}`);
+      loadMeetups();
+      Alert.alert('Aviso', 'Incrição cancelada');
+    } catch (error) {
+      Alert.alert('Aviso', 'Houve um erro ao tentar cancelar.');
+    }
+  }
+
+  return (
+    <Background>
+      <Header />
+
+      <List
+        data={meetups}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <Card
+            data={item}
+            handleClickButton={() => handleCancel(item.id)}
+            textButton="Cancelar Inscrição"
+          />
+        )}
+      />
+    </Background>
+  );
 }
 
 Subscriptions.navigationOptions = {
---
+++ src/pages/Subscriptions/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const List = styled.FlatList.attrs({
+  showVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 20 },
+})``;
